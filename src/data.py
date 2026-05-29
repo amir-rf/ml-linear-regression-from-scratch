ea1057b7f2bd3675618bf8540b967ce6cb5a770a
Amir RF <37160146+amir-rf@users.noreply.github.com>
2026-05-28 20:32:01 -0400

    Improve data loading utilities

--- src/data.py
+++ src/data.py
@@ -1,4 +1,28 @@
-- load California Housing dataset
-- convert it into a pandas DataFrame
-- separate features X and target y
-- split into train and test data
+from sklearn.datasets import fetch_california_housing
+from sklearn.model_selection import train_test_split
+import pandas as pd
+
+def load_california_housing():
+    dataset = fetch_california_housing(as_frame=True)
+
+    X = dataset.data
+    y = dataset.target
+
+    return X, y
+
+def split_data(X, y, test_size=0.2, random_state=42,):
+    return train_test_split(X, y,
+                            test_size=test_size,
+                            random_state=random_state)
+
+
+if __name__ == "__main__":
+    X, y = load_california_housing()
+
+    X_train, X_test, y_train, y_test = split_data(X, y)
+    print("Training features:", X_train.shape)
+    print("Testing features:", X_test.shape)
+    print("Training targets:", y_train.shape)
+    print("Testing targets:", y_test.shape)
+    print("\nFeature names:")
+    print(list(X.columns))
